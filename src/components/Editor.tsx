ffd6ddaa2ce52a0ac287f7c1679ffc3f40c33669
Sam Young <truong.vee@gmail.com>
2025-03-23 08:46:11 -0700

    disable add notes

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -33,7 +33,7 @@ const Editor = () => {
           style={{ whiteSpace: "pre-wrap" }}
         />
         <div className="items-end">
-          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Note</button>
+          <button type="button" className="text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center" disabled>Add Note</button> (unavailable for now)
         </div>
       </div>
     </div>
