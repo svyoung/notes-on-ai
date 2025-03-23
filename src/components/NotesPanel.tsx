eec7a3fee436f089fe0d85805a0df084c5d03539
Sam Young <truong.vee@gmail.com>
2025-03-23 12:14:30 -0700

    add number of notes

--- src/components/NotesPanel.tsx
+++ src/components/NotesPanel.tsx
@@ -18,7 +18,7 @@ export default function NotesPanel({ notes, searchFilterOn, selectNote, newNote,
     return (
         <div className="p-3 mb-[2rem]">
             <div className="flex flex-row justify-between items-center sticky">
-                <h1 className="text-[2rem]">Notes</h1>
+                <h1 className="text-[2rem]">Notes <span className="text-[1.1rem]">({notes.length})</span></h1>
                 {searchFilterOn && <button onClick={clearSearch} className="px-2 border border-[black] rounded-[50px] cursor-pointer text-white bg-black hover:bg-white hover:text-black">Clear search</button>}
                 <AddCircleIcon className="cursor-pointer transition delay-50 duration-600 ease-in-out hover:scale-150" sx={{ fontSize: "2rem"}} onClick={newNote} />
             </div>
